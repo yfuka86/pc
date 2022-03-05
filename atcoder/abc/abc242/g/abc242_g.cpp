929628ff8db0b1149f29b9f52a005da88d271923
Yuta Fukazawa <yuta@appbrew.io>
2022-03-06 00:26:20 +0900

    abc242

---
+++ atcoder/abc/abc242/g/abc242_g.cpp
@@ -0,0 +1,68 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+#define rep_r(i,n) for(ll i=(ll)(n)-1;i>=0;i--)
+#define rep2(i,sta,n) for(ll i=sta;i<(ll)(n);i++)
+#define rep2_r(i,sta,n) for(ll i=(ll)(n)-1;i>=sta;i--)
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef long double ld;
+typedef pair<int, int> P; typedef pair<ll, ll> LP;
+typedef vector<int> vi; typedef vector<ll> vl; typedef vector<LP> vlp; typedef vector<bool> vb; typedef vector<string> vs;
+const int INF = numeric_limits<int>::max();
+const ll LINF = LLONG_MAX;
+const double DINF = numeric_limits<double>::infinity();
+
+int ceil_pow2(ll n) { int x = 0; while ((1ULL << x) < (unsigned long long)(n)) x++; return x; }
+int floor_pow2(ll n) { int x = 0; while ((1ULL << (x + 1)) <= (unsigned long long)(n)) x++; return x; }
+template <typename T, typename S> T ceil(T x, S y) { assert(y); return (y < 0 ? ceil(-x, -y) : (x > 0 ? (x + y - 1) / y : x / y)); }
+template <typename T, typename S> T floor(T x, S y) { assert(y); return (y < 0 ? floor(-x, -y) : (x > 0 ? x / y : (x - y + 1) / y)); }
+template <class T> T POW(T x, int n) { assert(n >= 0); T res = 1; for(; n; n >>= 1, x *= x) if(n & 1) res *= x; return res; }
+template<typename T> void comp(vector<T>&a){ vector<T> b = a; sort(all(b)); b.erase(unique(all(b)), b.end()); rep(i, a.size()) a[i] = lower_bound(all(b), a[i]) - b.begin(); }
+template<typename T> void coutarray(vector<T>& v, int offset = 0) { rep(i, v.size()) { if (i > 0) cout << " "; cout << v[i] + offset; } cout << "\n"; }
+template<typename T> void coutmatrix(vector<vector<T>>& v) { rep(i, v.size()) { rep(j, v[i].size()) { if (j > 0) cout << " "; cout << v[i][j]; } cout << "\n";} }
+template<typename K, typename V> void coutmap(map<K, V> & m) { for (const auto& kv : m) { cout << kv.first << ":" << kv.second << " "; } cout << "\n"; }
+template<typename T> void coutbin(T &a, int d) { for (int i = d - 1; i >= 0; i--) cout << ((a >> i) & (T)1); cout << "\n"; }
+template<class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1;} return 0; }
+template<class T> bool chmax(T &a, const T &b) { if (b > a) { a = b; return 1;} return 0; }
+
+void solve() {
+  ll N; cin >> N;
+  vl a(N); rep(i, N) cin >> a[i];
+
+  map<ll, ll> temp;
+  vl fr(N), ba(N), oddfr(N), oddba(N), sumfr(N + 1, 0), sumba(N + 1, 0), sumofr(N + 1, 0), sumoba(N + 1, 0);
+
+  rep(i, N) {
+    temp[a[i]]++;
+    if (temp[a[i]] % 2 == 0) fr[i] = 1; else oddfr[i] = 1;
+  }
+  rep(i, N) {
+    temp[a[i]]--;
+    if (temp[a[i]] % 2 == 1) ba[i] = 1; else oddba[i] = 1;
+  }
+
+  rep(i, N) {
+    sumfr[i + 1] = sumfr[i] + fr[i];
+    sumba[i + 1] = sumba[i] + ba[i];
+    sumofr[i + 1] = sumofr[i] + oddfr[i];
+    sumoba[i + 1] = sumoba[i] + oddba[i];
+  }
+
+  ll Q; cin >> Q;
+  rep(i, Q) {
+    ll l, r; cin >> l >> r; l--; r--;
+    cout << min(sumfr[r + 1], sumofr[r + 1]) - min(sumoba[l], sumba[l]) << "\n";
+  }
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t = 1; // cin >> t;
+  while (t--) solve();
+}
