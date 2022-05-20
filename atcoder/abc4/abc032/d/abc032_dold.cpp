b43da63661e45189cb6923fd1b9acb0b839ddc01
Yuta Fukazawa <yuta@appbrew.io>
2022-05-21 04:46:03 +0900

    abc032

---
+++ atcoder/abc4/abc032/d/abc032_dold.cpp
@@ -0,0 +1,88 @@
+#pragma GCC optimize("Ofast")
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <climits>
+#include <bitset>
+#include <numeric>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,sta,n) for(int i=sta;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define per1(i,n) for(int i=n;i>=1;i--)
+#define Rep1(i,sta,n) for(int i=sta;i<=n;i++)
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define vi vector<int>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> P;
+typedef pair<ll, ll> LP;
+const int INF = numeric_limits<int>::max();
+const ll LINF = LLONG_MAX;
+const double DINF = numeric_limits<double>::infinity();
+const ll MOD = 1000000007;
+
+int bitcount(int n) { int c = 0; for ( ; n != 0 ; n &= n - 1 ) c++; return c;}
+void comp(vector<int>&a){ vector<int> b = a; sort(all(b)); b.erase(unique(all(b)), b.end()); rep(i, a.size()) a[i] = lower_bound(all(b), a[i]) - b.begin(); }
+template<typename T>
+void coutarray(vector<T>& v) { rep(i, v.size()) { if (i > 0) cout << " "; cout << v[i];} cout << "\n"; }
+template<typename K, typename V>
+void coutmap(map<K, V> & m) { for (const auto& kv : m) { cout << kv.first << ":" << kv.second << " "; } cout << "\n"; }
+template<typename T>
+void coutmatrix(vector<vector<T>>& v) { rep(i, v.size()) { rep(j, v[i].size()) { if (j > 0) cout << " "; cout << v[i][j]; } cout << "\n";} };
+template<class T>
+bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1;} return 0; }
+template<class T>
+bool chmax(T &a, const T &b) { if (b > a) { a = b; return 1;} return 0; }
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, W;
+  cin >> N >> W;
+  vector<ll> v(N), w(N);
+  rep(i, N) cin >> v[i] >> w[i];
+
+  map<ll, ll> dp;
+  dp[0] = 0;
+
+  rep(i, N) {
+    vector<LP> new_kv;
+    for (const auto& [key, val] : dp) {
+      ll nk = key + w[i];
+      ll nv = val + v[i];
+      if (dp.count(nk) && dp[nk] > nv) continue;
+      new_kv.pb(make_pair(nk, nv));
+    }
+    for (LP lp: new_kv) dp[lp.first] = lp.second;
+
+    auto itr = dp.begin();
+    ll v_sofar = -1;
+    while (itr != dp.end()) {
+      if (itr->second <= v_sofar || itr->first > W) {
+        itr = dp.erase(itr);
+      } else {
+        v_sofar = itr->second;
+        ++itr;
+      }
+    }
+    // coutmap(dp);
+  }
+
+  auto itr = dp.upper_bound(W);
+  itr--;
+  cout << itr->second << "\n";
+}
+
