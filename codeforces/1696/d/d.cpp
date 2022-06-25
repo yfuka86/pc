1028b2051fa91caa150600c6a832e1f1036e1208
Yuta Fukazawa <yuta@appbrew.io>
2022-06-26 04:49:53 +0900

    cf1696

--- codeforces/1696/d/d.cpp
+++ codeforces/1696/d/d.cpp
@@ -82,71 +82,63 @@ void compare(bool check = true) { RandGen rg; ll c = 0, loop = 10;
   }
 }
 
-//2021/12/22-ac----------------------------------------------------------------
-template <class S, S (*op)(S, S), S (*e)(), class F, S (*mapping)(F, S), F (*composition)(F, F), F (*id)()>
-struct lazy_segtree {
+//------------------------------------------------------------------------------
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
   public:
-  lazy_segtree() : lazy_segtree(0) {}
-  explicit lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
-  explicit lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) { log = ceil_pow2(_n); size = 1 << log; d = std::vector<S>(2 * size, e()); lz = std::vector<F>(size, id()); for (int i = 0; i < _n; i++) d[size + i] = v[i]; for (int i = size - 1; i >= 1; i--) update(i); }
-  void set(int p, S x) { assert(0 <= p && p < _n); p += size; for (int i = log; i >= 1; i--) push(p >> i); d[p] = x; for (int i = 1; i <= log; i++) update(p >> i); }
-  S get(int p) { assert(0 <= p && p < _n); p += size; for (int i = log; i >= 1; i--) push(p >> i); return d[p]; }
-  S prod(int l, int r) { assert(0 <= l && l <= r && r <= _n); if (l == r) return e(); l += size; r += size; for (int i = log; i >= 1; i--) { if (((l >> i) << i) != l) push(l >> i); if (((r >> i) << i) != r) push((r - 1) >> i); } S sml = e(), smr = e();
-    while (l < r) { if (l & 1) sml = op(sml, d[l++]); if (r & 1) smr = op(d[--r], smr); l >>= 1; r >>= 1; } return op(sml, smr); }
-  S all_prod() { return d[1]; }
-  void apply(int p, F f) { assert(0 <= p && p < _n); p += size; for (int i = log; i >= 1; i--) push(p >> i); d[p] = mapping(f, d[p]); for (int i = 1; i <= log; i++) update(p >> i); }
-  void apply(int l, int r, F f) { assert(0 <= l && l <= r && r <= _n); if (l == r) return; l += size; r += size; for (int i = log; i >= 1; i--) { if (((l >> i) << i) != l) push(l >> i); if (((r >> i) << i) != r) push((r - 1) >> i); }
-    { int l2 = l, r2 = r; while (l < r) { if (l & 1) all_apply(l++, f); if (r & 1) all_apply(--r, f); l >>= 1; r >>= 1; } l = l2; r = r2; } for (int i = 1; i <= log; i++) { if (((l >> i) << i) != l) update(l >> i); if (((r >> i) << i) != r) update((r - 1) >> i); } }
-  template <bool (*g)(S)> int max_right(int l) { return max_right(l, [](S x) { return g(x); }); }
-  template <class G> int max_right(int l, G g) { assert(0 <= l && l <= _n); assert(g(e())); if (l == _n) return _n; l += size; for (int i = log; i >= 1; i--) push(l >> i); S sm = e();
-    do { while (l % 2 == 0) l >>= 1; if (!g(op(sm, d[l]))) { while (l < size) { push(l); l = (2 * l); if (g(op(sm, d[l]))) { sm = op(sm, d[l]); l++; } } return l - size; } sm = op(sm, d[l]); l++; } while ((l & -l) != l); return _n; }
-  template <bool (*g)(S)> int min_left(int r) { return min_left(r, [](S x) { return g(x); }); }
-  template <class G> int min_left(int r, G g) { assert(0 <= r && r <= _n); assert(g(e())); if (r == 0) return 0; r += size; for (int i = log; i >= 1; i--) push((r - 1) >> i); S sm = e();
-    do { r--; while (r > 1 && (r % 2)) r >>= 1; if (!g(op(d[r], sm))) { while (r < size) { push(r); r = (2 * r + 1); if (g(op(d[r], sm))) { sm = op(d[r], sm); r--; } } return r + 1 - size; } sm = op(d[r], sm); } while ((r & -r) != r); return 0; }
+  segtree() : segtree(0) {}
+  explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+  explicit segtree(const std::vector<S>& v) : _n(int(v.size())) { log = ceil_pow2(_n); size = 1 << log; d = std::vector<S>(2 * size, e()); for (int i = 0; i < _n; i++) d[size + i] = v[i]; for (int i = size - 1; i >= 1; i--) update(i); }
+  void set(int p, S x) { assert(0 <= p && p < _n); p += size; d[p] = x; for (int i = 1; i <= log; i++) update(p >> i); }
+  S get(int p) const { assert(0 <= p && p < _n); return d[p + size]; }
+  S prod(int l, int r) const { assert(0 <= l && l <= r && r <= _n); S sml = e(), smr = e(); l += size; r += size; while (l < r) { if (l & 1) sml = op(sml, d[l++]); if (r & 1) smr = op(d[--r], smr); l >>= 1; r >>= 1; } return op(sml, smr); }
+  S all_prod() const { return d[1]; }
+  template <bool (*f)(S)> int max_right(int l) const { return max_right(l, [](S x) { return f(x); }); }
+  template <class F> int max_right(int l, F f) const { assert(0 <= l && l <= _n); assert(f(e())); if (l == _n) return _n; l += size; S sm = e();
+    do { while (l % 2 == 0) l >>= 1; if (!f(op(sm, d[l]))) { while (l < size) { l = (2 * l); if (f(op(sm, d[l]))) { sm = op(sm, d[l]); l++; } } return l - size; } sm = op(sm, d[l]); l++; } while ((l & -l) != l); return _n; }
+  template <bool (*f)(S)> int min_left(int r) const { return min_left(r, [](S x) { return f(x); }); }
+  template <class F> int min_left(int r, F f) const { assert(0 <= r && r <= _n); assert(f(e())); if (r == 0) return 0; r += size; S sm = e();
+    do { r--; while (r > 1 && (r % 2)) r >>= 1; if (!f(op(d[r], sm))) { while (r < size) { r = (2 * r + 1); if (f(op(d[r], sm))) { sm = op(d[r], sm); r--; } } return r + 1 - size; } sm = op(d[r], sm); } while ((r & -r) != r); return 0; }
   private:
-  int _n, size, log; std::vector<S> d; std::vector<F> lz;
+  int _n, size, log; std::vector<S> d;
   void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
-  void all_apply(int k, F f) { d[k] = mapping(f, d[k]); if (k < size) lz[k] = composition(f, lz[k]); }
-  void push(int k) { all_apply(2 * k, lz[k]); all_apply(2 * k + 1, lz[k]); lz[k] = id(); }
 };
+//------------------------------------------------------------------------------
 
-using S = ll; using F = ll;
-S op(S l, S r) { return min(l, r); }
-S e() { return LINF; }
-S mapping(F f, S x) { return f != -1 ? min(x, f) : x; }
-F composition(F f, F g) {
-  if (f == -1) {
-    return g;
-  } else {
-    return g == -1 ? f : min(f, g);
-  }
-}
-F id() { return -1; }
+using S = ll;
+S miop(S l, S r) { return min(l, r); }
+S mie() { return LINF; }
 
-#include <atcoder/string>
-using namespace atcoder;
+S maop(S l, S r) { return max(l, r); }
+S mae() { return -LINF; }
+
+
 
 void solve() {
-  string s, t; cin >> s >> t;
-  auto lcpl = z_algorithm(s + t);
-  // coutarray(lcpl);
-  ll sn = s.size(), tn = t.size();
+  ll n; cin >> n;
+  vlin(a, n, 1);
+  vl inv(n);
+  rep(i, n) inv[a[i]] = i;
 
-  lazy_segtree<S, op, e, F, mapping, composition, id> seg(tn + 1);
-  seg.set(0, 0);
-  rep(i, tn) {
-    if (seg.get(i) == LINF) continue;
-    ll len = min<ll>(sn, lcpl[sn + i]);
-    if (len) seg.apply(i + 1, i + 1 + len, seg.get(i) + 1);
+  segtree<S, miop, mie> segmi(a);
+  segtree<S, maop, mae> segma(a);
+
+  ll cur = 0, ans = 0;
+  while (cur < n - 1) {
+    ll mari = segma.max_right<function<bool(S)>>(cur, [&](S x) { return x <= a[cur]; });
+    ll miidx = cur == mari ? cur : inv[segmi.prod(cur, mari)];
+    ll miri = segmi.max_right<function<bool(S)>>(cur, [&](S x) { return x >= a[cur]; });
+    ll maidx = cur == miri ? cur : inv[segma.prod(cur, miri)];
+    // cout << mari << " " << miidx << " " << miri << " " << maidx << "\n";
+    ans++;
+    cur = max(cur + 1, max(miidx, maidx));
   }
-
-  if (seg.get(tn) == LINF) cout << -1 << "\n"; else cout << seg.get(tn) << "\n";
+  cout << ans << "\n";
 }
 
 signed main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr); cout.tie(nullptr); cout << fixed << setprecision(15);
-  int t = 1; // cin >> t;
+  int t; cin >> t;
   while (t--) solve();
   // while (t--) compare();
 }
