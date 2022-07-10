114f939d1306c760e5505c0d08635c37b2dd11c1
Yuta Fukazawa <yuta@appbrew.io>
2022-07-10 23:36:36 +0900

    abc259

---
+++ atcoder/abc/abc259/b/abc259_b.cpp
@@ -0,0 +1,250 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define rep(i,n) for(ll i=0;i<(ll)(n);i++)
+#define rep_r(i,n) for(ll i=(ll)(n)-1;i>=0;i--)
+#define rep2(i,sta,n) for(ll i=sta;i<(ll)(n);i++)
+#define rep2_r(i,sta,n) for(ll i=(ll)(n)-1;i>=sta;i--)
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define vlin(name,sz,offset) vl name(sz); rep(i,sz){cin>>name[i]; name[i]-=offset;}
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+using namespace std;
+typedef long long ll; typedef unsigned long long ull; typedef long double ld;
+typedef pair<int, int> P; typedef pair<ll, ll> LP; typedef map<ll, ll> LM; typedef tuple<ll, ll, ll> LT; typedef tuple<ll, ll, ll, ll> LT4;
+typedef vector<int> vi; typedef vector<vi> vvi; typedef vector<ll> vl; typedef vector<vl> vvl; typedef vector<vvl> v3l; typedef vector<v3l> v4l; typedef vector<v4l> v5l;
+typedef vector<LP> vlp; typedef vector<vlp> vvlp; typedef vector<LT> vlt; typedef vector<vlt> vvlt; typedef vector<string> vs; typedef vector<vs> vvs;
+typedef vector<ld> vd; typedef vector<vd> vvd; typedef vector<bool> vb; typedef vector<vb> vvb;
+template<typename T> class infinity{ public: static constexpr T MAX=numeric_limits<T>::max(); static constexpr T MIN=numeric_limits<T>::min(); static constexpr T val=numeric_limits<T>::max()/2-1e6; static constexpr T mval=numeric_limits<T>::min()/2+1e6; };
+const int INF = infinity<int>::val; const ll LINF = infinity<ll>::val; const ld DINF = infinity<ld>::val;
+
+struct RandGen {
+  using ud = uniform_int_distribution<ll>; mt19937 mt; RandGen() : mt(chrono::steady_clock::now().time_since_epoch().count()) {}
+  ll l(ll a, ll b) { ud d(a, b - 1); return d(mt); }
+  LP lp(ll a, ll b, bool rng = true) { ll x = l(a, b - 1), y = l(rng ? x + 1 : a, b - 1); return {x, y}; }
+  vl vecl(ll n, ll a, ll b) { ud d(a, b - 1); vl ret(n); rep(i, n) ret[i] = d(mt); return ret; }
+  vl vecperm(ll n, ll from = 0) { vl perm(n); iota(all(perm), from); shuffle(perm); return perm; }
+  string str(ll n, string op) { vl fig = vecl(n, 0, op.size()); string s; rep(i, n) s.pb(op[fig[i]]); return s; }
+  string straz(ll n, ll a = 0, ll z = 26) { vl az = vecl(n, a, z); string s; rep(i, n) s.pb('a' + az[i]); return s; }
+  string strnum(ll n, ll zero = 0, ll ten = 10) { vl zt = vecl(n, zero, ten); string s; rep(i, n) s.pb('0' + zt[i]); return s; }
+  void shuffle(vl &a) { std::shuffle(all(a), mt); }
+};
+
+#define dout cout
+template<typename T, typename=void> struct is_specialize:false_type{};
+template<typename T> struct is_specialize<T, typename conditional<false,typename T::iterator, void>::type>:true_type{};
+template<typename T> struct is_specialize<T, typename conditional<false,decltype(T::first), void>::type>:true_type{};
+template<typename T> struct is_specialize<T, enable_if_t<is_integral<T>::value, void>>:true_type{};
+void dump(const char &t) { dout<<t; } void dump(const string &t) { dout<<t; } void dump(const bool &t) { dout<<(t ? "true" : "false"); }
+template <typename T, enable_if_t<!is_specialize<T>::value, nullptr_t> =nullptr> void dump(const T&t) { dout<<t; }
+template<typename T> void dump(const T&t, enable_if_t<is_integral<T>::value>* =nullptr) { string tmp;if(t==infinity<T>::val||t==infinity<T>::MAX)tmp="inf";if(is_signed<T>::value&&(t==infinity<T>::mval||t==infinity<T>::MIN))tmp="-inf";if(tmp.empty())tmp=to_string(t);dout<<tmp; }
+template<typename T,typename U> void dump(const pair<T,U>&);
+template<typename T> void dump(const T&t, enable_if_t<!is_void<typename T::iterator>::value>* =nullptr) { dout << "{ "; for(auto it=t.begin();it!=t.end();){ dump(*it); dout << (++it==t.end() ? "" : " "); } dout<<" }"; }
+template<typename T,typename U> void dump(const pair<T,U>&t) { dout<<"("; dump(t.first); dout<<" "; dump(t.second); dout << ")"; }
+void trace() { dout << "\n"; } template<typename Head, typename... Tail> void trace(Head&&head, Tail&&... tail) { dump(head); if(sizeof...(tail)) dout<<", "; trace(forward<Tail>(tail)...); }
+#ifdef ONLINE_JUDGE
+#define debug(...) (void(0))
+#else
+#define debug(...) do {dout<<#__VA_ARGS__<<" = ";trace(__VA_ARGS__); } while(0)
+#endif
+template<typename T> void coutarray(vector<T>& v, int offset = 0, string sep = " ") { rep(i, v.size()) { if (i > 0) cout << sep; if (offset) cout << v[i] + offset; else cout << v[i]; } cout << "\n"; }
+template<typename T> void coutmatrix(vector<vector<T>>& v, int offset = 0) { rep(i, v.size()) { xcoutarray(v[i], offset); } }
+template<typename T> void coutbin(T &a, int d) { for (int i = d - 1; i >= 0; i--) cout << ((a >> i) & (T)1); cout << "\n"; }
+template<typename Q, typename A> void iquery(initializer_list<Q> q, A &a, string str = "? ") { cout << str; vector<Q> v(q); coutarray(v); cout.flush(); cin >> a; }
+// template<typename Q, typename A> void iquery(initializer_list<Q> q, A &a, string str = "? ") { vector<Q> query(q); RandGen rg;
+//   a = query[0] ? A() : A();
+// }
+template<typename A> void ianswer(A a, string str = "! ") { cout << str << a << "\n"; cout.flush(); }
+
+int ceil_pow2(ll n) { int x = 0; while ((1ULL << x) < (unsigned long long)(n)) x++; return x; }
+int floor_pow2(ll n) { int x = 0; while ((1ULL << (x + 1)) <= (unsigned long long)(n)) x++; return x; }
+ll digits(ll n) { ll ret = 0; while(n > 0) { ret++; n /= 10; } return ret; }
+ll POW(ll x, int n) { assert(n >= 0); ll res = 1; for(; n; n >>= 1, x *= x) if(n & 1) res *= x; return res; }
+ll sqrt_ceil(ll x) { ll l = -1, r = x; while (r - l > 1) { ll m = (l + r) / 2; if (m * m >= x) r = m; else l = m; } return r; }
+template<typename T, typename S> T ceil(T x, S y) { assert(y); return (y < 0 ? ceil(-x, -y) : (x > 0 ? (x + y - 1) / y : x / y)); }
+template<typename T, typename S> T floor(T x, S y) { assert(y); return (y < 0 ? floor(-x, -y) : (x > 0 ? x / y : (x - y + 1) / y)); }
+template<typename T> void uniq(vector<T>&a) { sort(all(a)); a.erase(unique(all(a)), a.end()); }
+template<typename T> void comp(vector<T>&a) { vector<T> b = a; uniq(b); rep(i, a.size()) a[i] = lower_bound(all(b), a[i]) - b.begin(); }
+template<class T> bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1;} return 0; }
+template<class T> bool chmax(T &a, const T &b) { if (b > a) { a = b; return 1;} return 0; }
+template<class T> int lbs(vector<T> &a, const T &b) { return lower_bound(all(a), b) - a.begin(); };
+template<class T> int ubs(vector<T> &a, const T &b) { return upper_bound(all(a), b) - a.begin(); };
+ll binary_search(function<bool(ll)> check, ll ok, ll ng) { assert(check(ok)); while (abs(ok - ng) > 1) { auto x = (ng + ok) / 2; if (check(x)) ok = x; else ng = x; } return ok; }
+template<class T> vector<T> csum(vector<T> &a) { vl ret(a.size() + 1, 0); rep(i, a.size()) ret[i + 1] = ret[i] + a[i]; return ret; }
+template<class S> vector<pair<S, int>> RLE(const vector<S> &v) { vector<pair<S, int>> res; for(auto &e : v) if(res.empty() or res.back().first != e) res.emplace_back(e, 1); else res.back().second++; return res; }
+vector<pair<char, int>> RLE(const string &v) { vector<pair<char, int>> res; for(auto &e : v) if(res.empty() or res.back().first != e) res.emplace_back(e, 1); else res.back().second++; return res; }
+const string drul = "DRUL"; vl dx = {1, 0, -1, 0}; vl dy = {0, 1, 0, -1};
+
+ll solve(ll N, vl a) {
+  ll ans = N - a[0]; return ans;
+}
+
+ll naive(ll N, vl a) {
+  ll ans = N + a[0]; return ans;
+}
+
+void compare(bool check = true) { RandGen rg; ll c = 0, loop = 10;
+  while (++c) { if (c % loop == 0) cout << "reached " << c / loop << "loop" <<  "\n", cout.flush();
+    ll N = 10;
+    vl a = rg.vecl(N, 1, 1e2);
+    auto so = solve(N, a); auto na = naive(N, a);
+    if (!check || na != so) { cout << c << "times tried" << "\n";
+      dump(N); dump(a);
+      cout << "solve: "; dump(so);
+      cout << "naive: "; dump(na);
+    if (check || (!check && c > loop)) break; }
+  }
+}
+
+// https://nyaannyaan.github.io/library/geometry/geometry.hpp
+
+using Real = long double;
+using Point = complex<Real>;
+using Points = vector<Point>;
+constexpr Real EPS = 1e-8;  // 問題によって変える！
+constexpr Real pi = 3.141592653589793238462643383279L;
+istream &operator>>(istream &is, Point &p) {
+  Real a, b;
+  is >> a >> b;
+  p = Point(a, b);
+  return is;
+}
+ostream &operator<<(ostream &os, Point &p) {
+  return os << real(p) << " " << imag(p);
+}
+inline bool eq(Real a, Real b) { return fabs(b - a) < EPS; }
+
+Point operator*(const Point &p, const Real &d) {
+  return Point(real(p) * d, imag(p) * d);
+}
+
+namespace std {
+bool operator<(const Point &a, const Point &b) {
+  return a.real() != b.real() ? a.real() < b.real() : a.imag() < b.imag();
+}
+}  // namespace std
+
+Real cross(const Point &a, const Point &b) {
+  return real(a) * imag(b) - imag(a) * real(b);
+}
+Real dot(const Point &a, const Point &b) {
+  return real(a) * real(b) + imag(a) * imag(b);
+}
+
+// 偏角ソート
+// 与えられた中心と点集合に対して、偏角のリストを返す
+vector<Real> arg_sort(Point &a , Points ps){
+  vector<Real> ret;
+  for(Point& p : ps) if(a != p) ret.push_back(atan2(p.imag() - a.imag(), p.real() - a.real()));
+  sort(begin(ret) , end(ret));
+  return ret;
+};
+
+// 以下は出現頻度少なめ
+// ccw 点の進行方向
+int ccw(const Point &a, Point b, Point c) {
+  b = b - a, c = c - a;
+  if (cross(b, c) > EPS) return +1;   // 反時計回り
+  if (cross(b, c) < -EPS) return -1;  // 時計回り
+  if (dot(b, c) < 0) return +2;       // c-a-bの順で一直線
+  if (norm(b) < norm(c)) return -2;   // a-b-cの順で一直線
+  return 0;                           // a-c-bの順で一直線
+}
+
+// a-bベクトルとb-cベクトルのなす角度のうち小さい方を返す
+// (ベクトル同士のなす角、すなわち幾何でいうところの「外角」であることに注意！)
+// rem. 凸包に対して反時計回りにこの関数を適用すると、
+// 凸包の大きさにかかわらず和が360度になる(いわゆる外角の和)(AGC021-B)
+Real get_angle(const Point &a, const Point &b, const Point &c) {
+  const Point v(b - a), w(c - b);
+  Real alpha = atan2(v.imag(), v.real()), beta = atan2(w.imag(), w.real());
+  if (alpha > beta) swap(alpha, beta);
+  Real theta = (beta - alpha);
+  return min(theta, 2 * acos(-1) - theta);
+}
+
+//　反時計回りである自己交差のない多角形のclass
+using Polygon = vector<Point>;
+
+// 凸包
+Polygon convex_hull(vector<Point> ps) {
+  int n = (int)ps.size(), k = 0;
+  if (n <= 2) return ps;
+  sort(ps.begin(), ps.end());
+  vector<Point> ch(2 * n);
+  // 反時計周りに凸包を構築していく
+  for (int i = 0; i < n; ch[k++] = ps[i++]) {
+    // 条件分岐内はwhile(k >= 2 && ccw(ch[k-2],ch[k-1],ps[i]) != 1)と等価
+    while (k >= 2 && cross(ch[k - 1] - ch[k - 2], ps[i] - ch[k - 1]) < EPS) --k;
+  }
+  for (int i = n - 2, t = k + 1; i >= 0; ch[k++] = ps[i--]) {
+    while (k >= t && cross(ch[k - 1] - ch[k - 2], ps[i] - ch[k - 1]) < EPS) --k;
+  }
+  ch.resize(k - 1);
+  return ch;
+}
+
+// 多角形の面積
+Real area(const Polygon &p) {
+  Real A = 0;
+  for (int i = 0; i < (int)p.size(); ++i) {
+    A += cross(p[i], p[(i + 1) % p.size()]);
+  }
+  return A * 0.5;
+}
+
+struct Circle {
+  Point p;
+  Real r;
+
+  Circle() = default;
+  Circle(Point _p, Real _r) : p(_p), r(_r) {}
+};
+
+using Circles = vector<Circle>;
+
+int intersect(Circle c1, Circle c2) {
+  if (c1.r < c2.r) swap(c1, c2);
+  Real d = abs(c1.p - c2.p);
+  if (c1.r + c2.r < d) return 4;
+  if (eq(c1.r + c2.r, d)) return 3;
+  if (c1.r - c2.r < d) return 2;
+  if (eq(c1.r - c2.r, d)) return 1;
+  return 0;
+}
+
+pair<Point, Point> crosspoint(const Circle &c1, const Circle &c2) {
+  Real d = abs(c1.p - c2.p);
+  Real x = (c1.r * c1.r + d * d - c2.r * c2.r) / (2 * c1.r * d);
+  if (abs(x) > 1) x = (x > 0 ? 1.0 : -1.0);
+  Real a = acos(x);
+  Real t = atan2(c2.p.imag() - c1.p.imag(), c2.p.real() - c1.p.real());
+  Point p1 = c1.p + Point(cos(t + a) * c1.r, sin(t + a) * c1.r);
+  Point p2 = c1.p + Point(cos(t - a) * c1.r, sin(t - a) * c1.r);
+  return {p1, p2};
+}
+
+
+void solve() {
+  ld a, b, d; cin >> a >> b >> d;
+
+  ld ang = d / 180.0 * M_PI;
+  // ld ad = cos(ang) * a - sin(ang) * b;
+  // ld bd = sin(ang) * a + cos(ang) * b;
+  // cout << ad << " " << bd << "\n";
+
+  Point p = {a, b};
+  p *= exp(Point{0, ang});
+  cout << p.real() << " " << p.imag() << "\n";
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr); cout << fixed << setprecision(15);
+  int t = 1; //cin >> t;
+  while (t--) solve();
+  // while (t--) compare();
+}
