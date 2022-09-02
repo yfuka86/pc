424ddfdee9d63b864dce1d447371214bfd4824a5
Yuta Fukazawa <yuta@appbrew.io>
2022-09-02 23:31:23 +0900

    before div2

--- temp/math/matrix.cpp
+++ temp/math/matrix.cpp
@@ -25,77 +25,117 @@ template<typename T> vector<vector<T>> m_product(vector<vector<T>> l, vector<vec
   return ans; }
 template <typename T> vector<vector<T>> m_pow(vector<vector<T>> m, ll n) { assert(n >= 0); if (!n) return m_e<T>((ll)m.size()); vector<vector<T>> res = m; n--; for(; n; n >>= 1, m = m_product(m, m)) if(n & 1) res = m_product(res, m); return res; }
 
+// https://nyaannyaan.github.io/library/matrix/matrix.hpp
+template <class T>
+struct Matrix {
+  vector<vector<T> > A;
 
-// https://judge.yosupo.jp/submission/91018
-template<class T>
-struct Matrix{
-  size_t N, M;
-  vector<T> a;
-  Matrix(size_t N, size_t M): N(N), M(M), a(N * M){}
-  Matrix(const vector<vector<T>>& b){
-    N = b.size(); M = b[0].size();
-    a.resize(N * M);
-    for(size_t i = 0; i < N; i++) for(size_t j = 0; j < M; j++) a[i * M + j] = b[i][j];
+  Matrix() = default;
+  Matrix(int n, int m) : A(n, vector<T>(m, T())) {}
+  Matrix(int n) : A(n, vector<T>(n, T())){};
+  Matrix(vector<vector<T>> &a) { A = a; }
+  int H() const { return A.size(); }
+  int W() const { return A[0].size(); }
+  int size() const { return A.size(); }
+  inline const vector<T> &operator[](int k) const { return A[k]; }
+  inline vector<T> &operator[](int k) { return A[k]; }
+  static Matrix I(int n) {
+    Matrix mat(n);
+    for (int i = 0; i < n; i++) mat[i][i] = 1;
+    return (mat);
   }
-  static Matrix e(size_t N){
-    Matrix a(N, N);
-    for(size_t i = 0; i < N; i++) a[i * (N + 1)] = T{1};
-    return a;
+  Matrix &operator+=(const Matrix &B) {
+    int n = H(), m = W();
+    assert(n == B.H() && m == B.W());
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++) (*this)[i][j] += B[i][j];
+    return (*this);
   }
-  auto operator[](size_t i){ return a.begin() + i * M; }
-  auto operator[](size_t i) const { return a.begin() + i * M; }
-  bool operator==(const Matrix& b){ return a == b.a; }
-  bool operator!=(const Matrix& b){ return a != b.a; }
-  Matrix& operator+=(const Matrix& b){ for(size_t i = 0; i < N * M; i++) a[i] += b[i]; return *this; }
-  Matrix operator+(const Matrix& b) const { return Matrix(*this) += b; }
-  Matrix& operator-=(const Matrix& b){ for(size_t i = 0; i < N * M; i++) a[i] -= b[i]; return *this; }
-  Matrix operator-(const Matrix& b) const { return Matrix(*this) -= b; }
-  Matrix operator*(const Matrix& b){
-    assert(M == b.N); const size_t K = b.M; Matrix c(N, K);
-    for(size_t i = 0; i < N; i++) for(size_t j = 0; j < M; j++) for(size_t k = 0; k < K; k++) c[i][k] += a[i * M + j] * b[j][k];
-    return c;
+  Matrix &operator-=(const Matrix &B) {
+    int n = H(), m = W();
+    assert(n == B.H() && m == B.W());
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < m; j++) (*this)[i][j] -= B[i][j];
+    return (*this);
   }
-  Matrix& operator*=(const Matrix& b){ return *this = a * b; }
-  Matrix pow(long long r) const {
-    if(r == 0) return e(N);
-    if(r < 0) return inv().pow(-r);
-    Matrix ans = e(N), A = *this;
-    while(1){ if(r & 1) ans *= A; if(r == 1) return ans; A *= A; r >>= 1; }
+  Matrix &operator*=(const Matrix &B) {
+    int n = H(), m = B.W(), p = W();
+    assert(p == B.H());
+    vector<vector<T> > C(n, vector<T>(m, T{}));
+    for (int i = 0; i < n; i++)
+      for (int k = 0; k < p; k++)
+        for (int j = 0; j < m; j++) C[i][j] += (*this)[i][k] * B[k][j];
+    A.swap(C);
+    return (*this);
   }
-  Matrix inv() const {
-    Matrix A = *this, B = e();
-    for(size_t i = 0; i < N; i++){
-      if(A[i][i] == 0) for(size_t j = i + 1; j < N; j++) if(A[j][i] != 0){
-        swap(A[i], A[j]); swap(B[i], B[j]); break;
-      }
-      assert("this matrix is not regular." && A[i][i] != 0);
-      const T x = T{1} / A[i][i];
-      for(size_t k = i; k < N; k++) A[i][k] *= x;
-      for(size_t k = 0; k < N; k++) B[i][k] *= x;
-      for(size_t j = 0; j < N; j++) if(i != j){
-        const T x = A[j][i];
-        for(size_t k = i; k < N; k++) A[j][k] -= A[i][k] * x;
-        for(size_t k = 0; k < N; k++) B[j][k] -= B[i][k] * x;
+  Matrix &operator^=(long long k) {
+    Matrix B = Matrix::I(H());
+    while (k > 0) {
+      if (k & 1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
+    }
+    A.swap(B.A);
+    return (*this);
+  }
+  Matrix operator+(const Matrix &B) const { return (Matrix(*this) += B); }
+  Matrix operator-(const Matrix &B) const { return (Matrix(*this) -= B); }
+  Matrix operator*(const Matrix &B) const { return (Matrix(*this) *= B); }
+  Matrix operator^(const long long k) const { return (Matrix(*this) ^= k); }
+  bool operator==(const Matrix &B) const {
+    assert(H() == B.H() && W() == B.W());
+    for (int i = 0; i < H(); i++)
+      for (int j = 0; j < W(); j++)
+        if (A[i][j] != B[i][j]) return false;
+    return true;
+  }
+  bool operator!=(const Matrix &B) const {
+    assert(H() == B.H() && W() == B.W());
+    for (int i = 0; i < H(); i++)
+      for (int j = 0; j < W(); j++)
+        if (A[i][j] != B[i][j]) return true;
+    return false;
+  }
+  friend ostream &operator<<(ostream &os, const Matrix &p) {
+    int n = p.H(), m = p.W();
+    for (int i = 0; i < n; i++) {
+      os << (i ? "   " : "") << "[";
+      for (int j = 0; j < m; j++) {
+        os << p[i][j] << (j + 1 == m ? "]\n" : ",");
       }
     }
-    return B;
+    return (os);
   }
-  T det() const {
-    Matrix A = *this; T ans = 1;
-    for(size_t i = 0; i < N; i++){
-      if(A[i][i] == 0) for(size_t j = i + 1; j < N; j++) if(A[j][i] != 0){
-        for(size_t k = i; k < N; k++) swap(A[i][k], A[j][k]);
-        ans = -ans; break;
+  T determinant() const {
+    Matrix B(*this);
+    assert(H() == W());
+    T ret = 1;
+    for (int i = 0; i < H(); i++) {
+      int idx = -1;
+      for (int j = i; j < W(); j++) {
+        if (B[j][i] != 0) {
+          idx = j;
+          break;
+        }
       }
-      if(A[i][i] == 0) return 0;
-      ans *= A[i][i];
-      const T x = T{1} / A[i][i];
-      for(size_t k = i; k < N; k++) A[i][k] *= x;
-      for(size_t j = i + 1; j < N; j++){
-        const T x = A[j][i];
-        for(size_t k = i; k < N; k++) A[j][k] -= A[i][k] * x;
+      if (idx == -1) return 0;
+      if (i != idx) {
+        ret *= T(-1);
+        swap(B[i], B[idx]);
+      }
+      ret *= B[i][i];
+      T inv = T(1) / B[i][i];
+      for (int j = 0; j < W(); j++) {
+        B[i][j] *= inv;
+      }
+      for (int j = i + 1; j < H(); j++) {
+        T a = B[j][i];
+        if (a == 0) continue;
+        for (int k = i; k < W(); k++) {
+          B[j][k] -= B[i][k] * a;
+        }
       }
     }
-    return ans;
+    return ret;
   }
 };
