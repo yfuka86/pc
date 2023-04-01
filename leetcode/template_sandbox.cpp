a0badcc5a5ea74403125f4b19df59c777624e6ba
Yuta Fukazawa <yuta@appbrew.io>
2023-04-01 20:58:40 +0900

    before abc296

---
+++ leetcode/template_sandbox.cpp
@@ -0,0 +1,65 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long; using uint = unsigned int; using ull = unsigned long long; using ld = long double; using i128 = __int128_t;
+using P = pair<int, int>; using LP = pair<ll, ll>; using LT = tuple<ll, ll, ll>; using LT4 =  tuple<ll, ll, ll, ll>;
+typedef vector<int> vi; typedef vector<vi> vvi; typedef vector<ll> vl; typedef vector<vl> vvl; typedef vector<vvl> v3l; typedef vector<v3l> v4l; typedef vector<v4l> v5l;
+typedef vector<LP> vlp; typedef vector<vlp> vvlp; typedef vector<LT> vlt; typedef vector<vlt> vvlt; typedef vector<LT4> vlt4; typedef vector<string> vs; typedef vector<vs> vvs;
+typedef vector<ld> vd; typedef vector<vd> vvd; typedef vector<bool> vb; typedef vector<vb> vvb;
+template<typename T> using pq = priority_queue<T>; template<typename T> using mpq = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> class infinity{ public: static constexpr T MAX=numeric_limits<T>::max(); static constexpr T MIN=numeric_limits<T>::min(); static constexpr T val=numeric_limits<T>::max()/2-1e6; static constexpr T mval=numeric_limits<T>::min()/2+1e6; };
+const int INF = infinity<int>::val; const ll LINF = infinity<ll>::val; const ld DINF = infinity<ld>::val;
+#define _overload5(a, b, c, d, e, name, ...) name
+#define _overload4(a, b, c, d, name, ...) name
+#define _overload3(a, b, c, name, ...) name
+#define _rep0(n) for(ll i = 0; i < (ll)(n); ++i)
+#define _rep1(i, n) for(ll i = 0; i < (ll)(n); ++i)
+#define _rep2(i, a, b) for(ll i = (ll)(a); i < (ll)(b); ++i)
+#define _rep3(i, a, b, c) for(ll i = (ll)(a); i < (ll)(b); i += (ll)(c))
+#define rep(...) _overload4(__VA_ARGS__, _rep3, _rep2, _rep1, _rep0)(__VA_ARGS__)
+#define _per0(n) for(ll i = 0; i < (ll)(n); ++i)
+#define _per1(i, n) for(ll i = (ll)(n)-1; i >= 0; --i)
+#define _per2(i, a, b) for(ll i = (ll)(b)-1; i >= (ll)(a); --i)
+#define _per3(i, a, b, c) for(ll i = (ll)(b)-1; i >= (ll)(a); i -= (ll)(c))
+#define rep_r(...) _overload4(__VA_ARGS__, _per3, _per2, _per1, _per0)(__VA_ARGS__)
+#define _fore0(a) rep(a.size())
+#define _fore1(i, a) for(auto &&i : a)
+#define _fore2(a, b, v) for(auto &&[a, b] : v)
+#define _fore3(a, b, c, v) for(auto &&[a, b, c] : v)
+#define _fore4(a, b, c, d, v) for(auto &&[a, b, c, d] : v)
+#define fore(...) _overload5(__VA_ARGS__, _fore4, _fore3, _fore2, _fore1, _fore0)(__VA_ARGS__)
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define rng_of(v, l, r) (v).begin() + l, (v).begin() + r
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define v3(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define v4(type, name, a, b, c, ...) vector<vector<vector<vector<type>>>> name(a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+template<typename T, typename S> T ceil(T x, S y) { assert(y); return (y < 0 ? ceil(-x, -y) : (x > 0 ? (x + y - 1) / y : x / y)); }
+template<typename T, typename S> T floor(T x, S y) { assert(y); return (y < 0 ? floor(-x, -y) : (x > 0 ? x / y : (x - y + 1) / y)); }
+template<class T, class U> bool chmin(T &a, const U &b) { if (b < a) { a = b; return 1;} return 0; }
+template<class T, class U> bool chmax(T &a, const U &b) { if (b > a) { a = b; return 1;} return 0; }
+
+vl ps(vl &c){
+  vl a, b;
+  rep(i, c.size() / 2) a.pb(c[i]);
+  rep(i, c.size() / 2, c.size()) b.pb(c[i]);
+  vl x;
+  rep(i, c.size() / 2) {
+    x.pb(a.back()); x.pb(b.back());
+    a.pop_back(); b.pop_back();
+  }
+  return x;
+}
+
+int main() {
+  vl cards(52); iota(all(cards), 0);
+  rep(_, 10) cards = ps(cards);
+  rep(i, 52) {
+    cout << cards[i] << '\n';
+  }
+}
